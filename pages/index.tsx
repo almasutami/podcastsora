83a9d820c10b375dcb0305d8a35ca01739f01c43
almasutami <86611956+almasutami@users.noreply.github.com>
2023-10-27 14:00:29 +0700

    fix UI

--- pages/index.tsx
+++ pages/index.tsx
@@ -362,6 +362,10 @@ export default function Home() {
             Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
             sans-serif;
         }
+        main {
+          padding: 0;
+          margin: 0;
+        }
         * {
           box-sizing: border-box;
         }
